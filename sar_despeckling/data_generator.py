3102ce55bbb2ed537224c191fb4f8c03d8c5a4ad
Tim <35193581+tonzowonzo@users.noreply.github.com>
2021-01-28 21:38:51 +0100

    updated model, and added fake sar noise to gen

--- sar_despeckling/data_generator.py
+++ sar_despeckling/data_generator.py
@@ -2,6 +2,9 @@ import cv2
 import os
 import random
 import numpy as np
+import scipy.ndimage
+from scipy import special
+import tensorflow as tf
 
 
 def load_image(image_path: str):
@@ -15,19 +18,49 @@ def load_image(image_path: str):
     img = cv2.imread(image_path, 0)
     return img
 
+
 def addsalt_pepper(img, SNR):
     img_ = img.copy()
     h, w = img_.shape
     mask = np.random.choice((0, 1, 2), size=(h, w), p=[SNR, (1 - SNR) / 2., (1 - SNR) / 2.])
-    img_[mask == 1] = 0.6 # salt noise
-    img_[mask == 2] = 0.2
+    img_[mask == 1] = 180
+    img_[mask == 2] = 40
     return img_
 
+
+def simulate_sar_noise(img: np.array) -> np.array:
+    """
+    Takes in a clean image and simulates SAR speckle on it. Based on:
+    https://gitlab.telecom-paris.fr/ring/SAR-CNN/-/blob/master/SAR_CNN_test.ipynb
+    Turned into Numpy code for ease of use.
+
+    :param img: A numpy array of a clean image.
+    :return: A speckled image.
+    """
+    # Set constants for noise creation.
+    M = 10.089038980848645
+    m = -1.429329123112601
+    L = 1
+    c = (1 / 2) * (special.psi(L) - np.log(L))
+    cn = c / (M - m)
+
+    s = tf.zeros(shape=tf.shape(img))
+
+    for k in range(0, L):
+        gamma = (np.abs(tf.complex(np.random.normal(size=img.shape),
+                                   np.random.normal(size=img.shape))) ** 2) / 2
+        s = s + gamma
+    s_amplitude = np.sqrt(s / L)
+    log_speckle = np.log(s_amplitude)
+    log_norm_speckle = log_speckle / (M - m)
+
+    noisy_im = img + log_norm_speckle
+    return noisy_im
 
 
 def generator():
     for i in range(5):
-        path = "C:/Users/tim.iles/Documents/Projects/sar_speckle_filtering/data/2750/"
+        path = "C:/Users/tim.iles/Documents/Projects/sar_speckle_filtering/data/big_dataset/NWPU-RESISC45/"
         image_paths = []
         # Get the paths of the images.
         for folder in os.listdir(path):
@@ -47,18 +80,22 @@ def generator():
 
             # Load image
             real_image = load_image(image_path)
+            real_image = cv2.resize(real_image, (64, 64))
             # Scale image between 0 and 1.
+
+            # # Create an image that's noisy.
+            # noise = np.random.uniform(low=-18, high=18, size=(64, 64))
+            #
+            # noisey_image = real_image.copy() + noise
+            # # Add salt + pepper noise too.
+            # noisey_image = addsalt_pepper(noisey_image, 0.9)
+
             real_image = real_image / 255
-
-            # Create an image that's noisy.
-            noise = np.random.uniform(low=-0.06, high=0.06, size=(64, 64))
-
-            noisey_image = real_image.copy() + noise
-            # Add salt + pepper noise too.
-            noisey_image = addsalt_pepper(noisey_image, 0.9)
-
+            noisey_image = simulate_sar_noise(real_image)
             real_image = real_image.reshape((1, 64, 64, 1))
             noisey_image = noisey_image.reshape((1, 64, 64, 1))
 
             X_stack[i % 32, :, :, :] = noisey_image
             y_stack[i % 32, :, :, :] = real_image
+
+
--- sar_despeckling/model.py
+++ sar_despeckling/model.py
@@ -1,34 +1,68 @@
-from keras.layers import Conv2D, MaxPool2D, Dense, Dropout, BatchNormalization, Input, UpSampling2D, concatenate
+from keras.layers import Conv2D, AveragePooling2D, Dense, Dropout, BatchNormalization, Input, UpSampling2D, concatenate
+from keras.layers import Lambda, GlobalAveragePooling2D
+import keras.backend as K
 from keras.models import Model
+
+def logFunc(x):
+    """
+    Takes the log of an input and returns it.
+
+    :param x:
+    :return:
+    """
+    return K.log(x)
+
+
+def expFunc(x):
+    """
+    Takes the inverse of a log and returns it.
+
+    :param x:
+    :return:
+    """
+    return K.exp(x)
 
 
 def build_model():
     input_1 = Input((64, 64, 1))
-    model = Conv2D(128, (3, 3), padding="same")(input_1)
-    conv1 = Conv2D(128, (3, 3), padding="same")(model)
-    model = MaxPool2D((2, 2))(conv1)
-    model = Dropout(0.4)(model)
+    model = Conv2D(64, 3, padding="same", activation="relu")(input_1)
 
-    model = Conv2D(64, (3, 3), padding="same", activation="relu")(model)
-    conv2 = Conv2D(64, (3, 3), padding="same", activation="relu")(model)
-    model = MaxPool2D((2, 2))(conv2)
-    model = Dropout(0.4)(model)
+    # Main convolutional block.
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
+    model = Conv2D(64, 3, padding="same", activation="relu", use_bias=False)(model)
+    model = BatchNormalization()(model)
 
-    up1 = UpSampling2D((2, 2))(model)
-    concat1 = concatenate([conv2, up1])
-    model = Conv2D(64, (3, 3), padding="same", activation="relu")(concat1)
-    model = Conv2D(64, (3, 3), padding="same", activation="relu")(model)
-
-    up2 = UpSampling2D((2, 2))(model)
-    concat2 = concatenate([conv1, up2])
-    model = Conv2D(128, (3, 3), padding="same", activation="relu")(concat2)
-    model = Conv2D(128, (3, 3), padding="same", activation="relu")(model)
-
-    model = Conv2D(256, (3, 3), padding="same", activation="relu")(model)
-    model = Conv2D(1, (1, 1))(model)
+    # Output layer.
+    model = Conv2D(1, 3, activation="linear", padding="same")(model)
     model = Model(inputs=[input_1], outputs=[model])
-
-    model.compile(optimizer="adam", metrics=["mse", "mae"], loss="mae")
+    model.compile(optimizer="adam", metrics=["mse", "mae"], loss="mse")
 
     return model
 
